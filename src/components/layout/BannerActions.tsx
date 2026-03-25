dca3faa226f7e2d278a2a834e2f49cff5d7ae02b
Dennis De St Jeor <dennis.destjeor@gmail.com>
2026-03-25 11:29:15 -0400

    login changed to sign in up

--- src/components/layout/BannerActions.tsx
+++ src/components/layout/BannerActions.tsx
@@ -99,7 +99,7 @@ export default function BannerActions() {
         )}
       </Link>
 
-      {/* Profile dropdown (logged in) or Login link (logged out) */}
+      {/* Profile dropdown (logged in) or sign-in link (logged out) */}
       {session?.user ? (
         <div className="relative" ref={dropdownRef}>
           <button
@@ -153,7 +153,7 @@ export default function BannerActions() {
           <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1" />
           </svg>
-          Login
+          Sign In/Up
         </Link>
       )}
     </div>
